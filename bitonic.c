370dafdd63bc12f78ef122d2e27fc61bbb0e8cce
David Kisluk <david.kisluk@gmail.com>
2017-11-05 21:04:04 -0500

    Print descending pairs

--- bitonic.c
+++ bitonic.c
@@ -7,6 +7,34 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+void to_bitonic_seq(int *array, int length) {
+  int num_stages = (int)(log2(length) - 1);
+
+  // Stages
+  for (int s = 1; s <= num_stages; s++) {
+    int step = (int)pow(2, s+1);
+    int comparisons_per_step = step / 2;
+
+    // Ranges?
+    for (int r = s; r > 0; r--) {
+      printf("Stage %d\tRound %d\n", s, r);
+      int pair_offset = (int)pow(2, r-1);
+
+      // Descending
+      for (int a = 0; a < length; a += step) {
+        int b = a + pair_offset;
+        int inner_step = (r > 1) ? 1 : 2;
+        for (int i = 0; i < comparisons_per_step/2; i++) {
+          printf("DESC(%d, %d)\n", a+i*inner_step, b+i*inner_step);
+        }
+      }
+
+      // Ascenging
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   /* Validate arguments */
@@ -25,14 +53,15 @@ int main(int argc, char *argv[]) {
   fscanf(pFile, "%d", &N);
 
   /* Parse array A */
-  double *A = malloc(sizeof(double[N]));
+  int *A = malloc(sizeof(int[N]));
 
   int e;
   for (int i=0; i<N; ++i) {
     fscanf(pFile, "%d", &e);
     A[i] = e;
-    printf("%d ", e);
   }
+
+  to_bitonic_seq(A, N);
 
   /* Close file */
   fclose(pFile);
